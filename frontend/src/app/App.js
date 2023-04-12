1e8a0cfe95728fe00cfbf99fc3d041e428d35deb
Vladik <v4054741@gmail.com>
2023-04-12 20:08:48 +0300

    create frontend

---
+++ frontend/src/app/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Home from '../home/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ClientList from '../client/ClientList';
+import ClientEdit from "../client/ClientEdit";
+
+class App extends Component {
+  render() {
+    return (
+        <Router>
+          <Switch>
+            <Route path='/' exact={true} component={Home}/>
+            <Route path='/clients' exact={true} component={ClientList}/>
+            <Route path='/clients/:id' component={ClientEdit}/>
+          </Switch>
+        </Router>
+    )
+  }
+}
+
+export default App;
